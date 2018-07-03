3172f1e3b42d2e74f5286df3af3ede4fce9f143c
Matthew Lin <mlin2814@gmail.com>
2018-07-03 15:02:33 -0400

    Updated logic to specify different lottos and with correct ranges for values

--- logic.py
+++ logic.py
@@ -1,5 +1,11 @@
 import random
 
-results = sorted(random.sample(range(1,50), 6))
+powerball_results = sorted(random.sample(range(1,69), 5))
+powerball_special = random.sample(range(1,26), 1)
 
-print(results)
+print("For the Powerball, pick: " + str(powerball_results) + " and the special number: " + str(powerball_special))
+
+mega_results = sorted(random.sample(range(1,70), 5))
+mega_special = random.sample(range(1,25), 1)
+
+print("For Megamillions, pick: " + str(mega_results) + " and the special number: " + str(mega_special))
